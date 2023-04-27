8f02e9b0cb07354bfa63e3fdea65414d05a9cb5d
franklindyer <franklin@dyer.me>
2023-04-27 21:33:33 +0200

    remove creds

---
+++ app.js
@@ -0,0 +1,19 @@
+const EchoService = require("./src/EchoService.js")
+const TalkTubeService = require("./src/TalkTubeService.js")
+const DiscordBot = require("./src/DiscordBot.js")
+const TelegramBot = require("./src/TelegramBot.js")
+const SlackBot = require("./src/SlackBot.js")
+
+tb = new TelegramBot("Mr. Telegram")
+db = new DiscordBot("Dr. Discord")
+sb = new SlackBot("Señor Slack")
+
+// es = new EchoService(sb)
+// es.run()
+
+// es2 = new EchoService(db)
+// es1.run()
+// es2.run()
+
+tts = new TalkTubeService(db, sb)
+tts.run()
